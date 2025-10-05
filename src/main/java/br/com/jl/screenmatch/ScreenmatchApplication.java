3262b976207147aad0265604189e3965969afb44
Lucas Nunes <lucas.jlnunes@gmail.com>
2025-10-05 20:48:31 -0300

    inicio projeto

---
+++ src/main/java/br/com/jl/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,28 @@
+package br.com.jl.screenmatch;
+
+import br.com.jl.screenmatch.model.DadosSerie;
+import br.com.jl.screenmatch.service.ConsumoApi;
+import br.com.jl.screenmatch.service.CoverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=d4daf175");
+	//	System.out.println(json);
+	//	json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+		CoverteDados conversor = new CoverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
