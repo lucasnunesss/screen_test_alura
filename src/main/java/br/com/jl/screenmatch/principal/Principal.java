ab123a59a5ab45ef24e267a28a1a885d9bd02543
JohnnyMarxx <johnny_dev_marx@outlook.com>
2025-10-19 19:07:57 -0400

    add episodes

--- src/main/java/br/com/jl/screenmatch/principal/Principal.java
+++ src/main/java/br/com/jl/screenmatch/principal/Principal.java
@@ -35,5 +35,12 @@ public class Principal {
 			temporadas.add(dadosTemporada);
 		}
 		temporadas.forEach(System.out::println);
+
+        for(int i = 0; i < dados.totalTemporadas; i++){
+            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            for(int j = 0; j < episodiosTemporada.size(); j++){
+                System.out.println(episodiosTemporada.get(j).titulo());
+            }
+        }
     }
 }
