5074f3047d77716951e91141ae0f2350da9d127e
JohnnyMarxx <johnny_dev_marx@outlook.com>
2025-10-19 19:13:47 -0400

    add new method

--- src/main/java/br/com/jl/screenmatch/principal/Principal.java
+++ src/main/java/br/com/jl/screenmatch/principal/Principal.java
@@ -37,11 +37,8 @@ public class Principal {
 		}
 		temporadas.forEach(System.out::println);
 
-        for(int i = 0; i < dados.totalTemporadas(); i++){
-            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-            for(int j = 0; j < episodiosTemporada.size(); j++){
-                System.out.println(episodiosTemporada.get(j).titulo());
-            }
-        }
+     
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
     }
 }
