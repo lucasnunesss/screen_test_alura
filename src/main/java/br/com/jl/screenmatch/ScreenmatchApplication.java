0efbfa04229735c13fc2652ff646fafe0a448131
JohnnyMarxx <johnny_dev_marx@outlook.com>
2025-10-19 15:37:22 -0400

    add temporadas

--- src/main/java/br/com/jl/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/jl/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,13 @@ package br.com.jl.screenmatch;
 
 import br.com.jl.screenmatch.model.DadosSerie;
 import br.com.jl.screenmatch.model.DadosEpisodio;
+import br.com.jl.screenmatch.model.DadosTemporada;
 import br.com.jl.screenmatch.service.ConsumoApi;
 import br.com.jl.screenmatch.service.CoverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,13 +23,22 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=d4daf175");
 	//	System.out.println(json);
 	//	json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
+		//System.out.println(json);
 		CoverteDados conversor = new CoverteDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		//System.out.println(dados);
 
 		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&episode=2&apikey=d4daf175");
 		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
+		// System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i<=dados.totalTemporadas; i++){
+			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=d4daf175");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
